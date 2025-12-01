a8127734448fba52da586fcb1b9d045dfae96904
7BZN <santisbernardo630@gmail.com>
2025-12-01 02:44:32 -0300

    tudo dando errado na minha vida

--- SeedTrace_backend/demo/src/main/java/com/seedtrace/api/repository/EntregaRepository.java
+++ SeedTrace_backend/demo/src/main/java/com/seedtrace/api/repository/EntregaRepository.java
@@ -1,6 +1,21 @@
 package com.seedtrace.api.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.seedtrace.api.model.Entrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface EntregaRepository extends JpaRepository<Entrega, Long> {}
+import java.util.List;
+
+public interface EntregaRepository extends JpaRepository<Entrega, Long> {
+	
+	 // 1) Distribuição por município
+    @Query("SELECT e.municipio AS municipio, SUM(e.quantidadeKg) AS totalKg " +
+           "FROM Entrega e GROUP BY e.municipio")
+    List<Object[]> relatorioDistribuicaoPorMunicipio();
+
+    // 2) Entregas por agricultor
+    @Query("SELECT e.agricultor AS agricultor, SUM(e.quantidadeKg) AS totalKg, COUNT(e) AS totalEntregas " +
+           "FROM Entrega e GROUP BY e.agricultor")
+    List<Object[]> relatorioEntregasPorAgricultor();
+	
+}
